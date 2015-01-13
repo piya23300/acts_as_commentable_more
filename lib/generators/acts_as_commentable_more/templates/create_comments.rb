fddcdf7bcaabc332553b2f80ed86117b3df2b4b1
Carryall <carryall369@gmail.com>
2015-01-13 16:10:15 +0700

    add create comment migration template

---
+++ lib/generators/acts_as_commentable_more/templates/create_comments.rb
@@ -0,0 +1,16 @@
+class <%= migration_class_name %> < ActiveRecord::Migration
+  def change
+    create_table :<%= table_name %> do |t|
+      # t.string :title, :limit => 50, :default => "" 
+      t.text :message
+      t.references :<%= class_name.demodulize.underscore + "able" %>, :polymorphic => true
+      t.references :user
+      t.string :type, :default => "comments"
+      t.timestamps
+    end
+
+    add_index :<%= table_name %>, :<%= class_name.demodulize.underscore + "able" %>_type
+    add_index :<%= table_name %>, :<%= class_name.demodulize.underscore + "able" %>_id
+    add_index :<%= table_name %>, :user_id
+  end
+end
