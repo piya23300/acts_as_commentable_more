ce04d4499592404f03d968a4e2055249b31a3f1a
Carryall <carryall369@gmail.com>
2015-01-13 16:09:39 +0700

    add comment model template

---
+++ lib/generators/acts_as_commentable_more/templates/comment.rb
@@ -0,0 +1,15 @@
+class <%= class_name %> < ActiveRecord::Base
+
+  include ActsAsCommentable::Comment
+
+  belongs_to :<%= class_name.demodulize.underscore + "able" %>, :polymorphic => true
+
+  default_scope -> { order('created_at ASC') }
+
+  # NOTE: install the acts_as_votable plugin if you
+  # want user to vote on the quality of comments.
+  #acts_as_votable
+
+  # NOTE: Comments belong to a user
+  belongs_to :user
+end
