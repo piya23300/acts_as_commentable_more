5053914467177a52ea52956263d7ed8840bcf225
Carryall <carryall369@gmail.com>
2015-01-14 11:51:06 +0700

    set user association to be polymorphic

--- lib/generators/commentable/templates/comment.rb
+++ lib/generators/commentable/templates/comment.rb
@@ -1,13 +1,7 @@
 class <%= class_name %> < ActiveRecord::Base
+  include ActsAsCommentableMore::Finders
 
   belongs_to :<%= class_name.demodulize.underscore + "able" %>, :polymorphic => true
+  belongs_to :user, polymorphic: true
 
-  default_scope -> { order('created_at ASC') }
-
-  # NOTE: install the acts_as_votable plugin if you
-  # want user to vote on the quality of comments.
-  #acts_as_votable
-
-  # NOTE: Comments belong to a user
-  belongs_to :user
 end
