506b3fb21f93fbefdb52a451311462633833bc66
Carryall <carryall369@gmail.com>
2015-01-14 15:42:30 +0700

    add instruction comment in model template resolves #3

--- lib/generators/commentable/templates/comment.rb
+++ lib/generators/commentable/templates/comment.rb
@@ -1,7 +1,17 @@
+
 class <%= class_name %> < ActiveRecord::Base
+  ###################################################################
+  ### To implement commentable add the following line to your model
+  ### acts_as_commentable types: [:hide, :show], options: { class_name: '<%= class_name %>', as: :<%= class_name.demodulize.underscore + "able" %> }
+  
+  ### types is an array of possible comment type
+  ### for example if you have public and private comment
+  ### your types would be [:public, :private]
+
   include ActsAsCommentableMore::Finders
 
   belongs_to :<%= class_name.demodulize.underscore + "able" %>, :polymorphic => true
   belongs_to :user, polymorphic: true
 
+  ###################################################################
 end
