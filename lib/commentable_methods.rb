122c5ad8d312f578eb45f9da298bcc2272cce17c
PiYa <piya23300@gmail.com>
2015-01-15 21:11:31 +0700

    add methods for add role comment resolves #15

--- lib/commentable_methods.rb
+++ lib/commentable_methods.rb
@@ -24,11 +24,21 @@ module Happio
           has_many "#{association_name.to_s}".to_sym,
                    -> { where(role: role.to_s) },
                    has_many_options(role, join_options)
+          define_add_role(association_name)
         end
 
         def has_many_options(role, join_options)
           { :before_add => Proc.new { |x, c| c.role = role.to_s } }.merge(join_options)
         end
+
+        def define_add_role(association_name)
+          class_eval %{
+            def add_#{association_name.to_s.singularize}(attributes = nil)
+              #{association_name.to_s}.create(attributes)
+            end
+          }
+        end
+
       end
 
       module ClassMethods
@@ -39,12 +49,15 @@ module Happio
           default_as = :comments
           default_roles = [default_as.to_s.singularize.to_sym]
 
+          types = types.flatten.compact.map(&:to_sym)
+
           association_options = default_options.merge(options.compact)
-          self.comment_roles = (default_roles + types.flatten.compact.map(&:to_sym)).uniq
+          self.comment_roles = types.present? ? types : default_roles
           association_base_name = (as || default_as).to_s.pluralize
 
           if comment_roles == [default_as.to_s.singularize.to_sym]
-            has_many association_base_name.to_sym, has_many_options(default_roles.first, association_options)
+            has_many association_base_name.to_sym, has_many_options(association_base_name.singularize, association_options)
+            define_add_role(association_base_name)
           else
             comment_roles.each do |role|
               define_role_based_inflection(role, association_base_name, association_options)
