b41e2778ab46638d948688c0cd92a50475c5c226
PiYa <piya23300@gmail.com>
2015-01-30 00:03:58 +0700

    refactor

--- lib/acts_as_commentable_more/helpers/comment/instance_methods_helper.rb
+++ lib/acts_as_commentable_more/helpers/comment/instance_methods_helper.rb
@@ -24,9 +24,9 @@ module ActsAsCommentableMore
               def find_comments_by_user(user, role = nil)
                 attr_finder = { user: user }
                 attr_finder.merge!(role: role.to_s) if role.present?
-                where(attr_finder).order("created_at DESC")
+                where(attr_finder).order(created_at: :desc)
               end
-              
+
             end
           end
 
